9265e39cdb7597ffd298e85589697c8df86ec5be
Muees99 <132849693+Muees99@users.noreply.github.com>
2023-09-24 07:24:06 -0700

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom';
 
 
 const bgStyles = {
-  backgroundImage:  class="bg-cover h-screen" style="background-image: url(https://images.unsplash.com/photo-1531315630201-bb15abeb1653?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80)",
+  backgroundImage: " url(https://images.unsplash.com/photo-1531315630201-bb15abeb1653?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80)",
   backgroundSize: 'cover',
   backgroundRepeat: 'no-repeat',
   backgroundPosition: 'center center'
